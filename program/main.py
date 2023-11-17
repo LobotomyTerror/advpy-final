f63407e7893582ffebcc7f9a21482ee2e43cc441
Daniel <dfishbein@mavs.coloradomesa.edu>
2023-11-16 18:13:44 -0700

    messing around with the api for getting movies

--- program/main.py
+++ program/main.py
@@ -3,6 +3,8 @@ import random
 from tmdbv3api import TMDb
 from tmdbv3api import Movie
 from tmdbv3api import Discover
+from tmdbv3api import Genre
+import requests
 
 tmdb = TMDb()
 tmdb.api_key = 'ddc68a76526100939a2f7c62b4f6b061'
@@ -11,15 +13,17 @@ tmdb.debug = True
 
 def get_movies_by_genre(genre: str, numOfMovies: int):
     discover = Discover()
+    movie_list = []
     movies = discover.discover_movies({
         'with_genres': f'{genre}',
         'sort_by': 'popularity.desc'
     })
-    print(f"{movies[0]}")
+    movie_list.append(movies)
+    print(f"{len(movie_list)}")
 
 
 def getMovies():
-    get_movies_by_genre('horror', 50)
+    get_movies_by_genre(27, 50)
 
 
 def main():
