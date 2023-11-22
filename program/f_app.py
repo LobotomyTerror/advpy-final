78e2ed92ec43517fc5a17834cea0310d99e4c289
Daniel <dfishbein@mavs.coloradomesa.edu>
2023-11-21 23:05:36 -0700

    added some new files and folders for webpage

--- program/f_app.py
+++ program/f_app.py
@@ -1,5 +1,5 @@
 from typing import Any
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 # add request and redirect to flas
 app = Flask(__name__)
@@ -8,6 +8,20 @@ app = Flask(__name__)
 @app.route('/')
 def index() -> Any:
     return render_template('index.html')
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def search() -> Any:
+    if request.method == 'POST':
+        search_query = request.form['searchInput']
+        return redirect(url_for('search_results', query=search_query))
+    return redirect(url_for('index'))
+
+
+@app.route('/search_results')
+def search_results():
+    search_query = request.args.get('query', '')
+    return render_template('search_results.html', query=search_query)
 
 
 if __name__ == "__main__":
