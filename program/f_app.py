1d031b7bb10e634885fe893b1535c4938e0a8d12
Daniel <dfishbein@mavs.coloradomesa.edu>
2023-11-29 11:40:38 -0700

    added some comments to explain what is happening

--- program/f_app.py
+++ program/f_app.py
@@ -1,3 +1,12 @@
+""" This file handles that actual deployment and rendering
+of the app itself. Where any search (POST) and clearing (GET)
+operations are handled and used to display the web app files
+(HTML/CSS).
+
+    Returns:
+        _type_: Any
+    """
+
 from typing import Any
 from flask import Flask, render_template, request, redirect, url_for
 from . import main
@@ -7,11 +16,35 @@ app = Flask(__name__)
 
 @app.route('/')
 def index() -> Any:
+    """This is the starting function for the application.
+    When the app is deployed it locates the templates file
+    in the root directory and then displays the 'index.html'
+    file that is located in the templates directory.
+
+    Returns:
+        Any: Render template just renders the HTML file
+        that is being returned (displays it on the screen)
+    """
     return render_template('index.html')
 
 
 @app.route('/search', methods=['GET', 'POST'])
 def search() -> Any:
+    """ This function waits for a POST to happen. When the
+    'index.html' file is rendered there is a search bar that
+    allows for stuff to be entered, once something is entered
+    the return acts as a submit in-turn a POST. When a query is
+    entered into the search bar the POST value (genre) is stored
+    into the request object and if it is a POST then we set the 
+    search_query equal to the the value of the request.form dict at
+    the given value. So search_query would be the (genre) string,
+    where we then redirect the app to the 'search_results' function
+    that renders the 'search_results.html' file.
+
+    Returns:
+        Any: Either a redirect to the search results function if
+        condition is true. Else we redirect to the index function
+    """
     if request.method == 'POST':
         search_query = request.form['searchInput']
         return redirect(url_for('search_results', query=search_query))
@@ -20,6 +53,21 @@ def search() -> Any:
 
 @app.route('/search_results')
 def search_results() -> Any:
+    """ When we reach this point the search_query then
+    gets the query result from request object or an empty
+    string if it doesn't match (this is passed this originally 
+    from the previous function above where query=search_query).
+    Once this happens then it runs through the main module, which
+    in-turn access the mongodb database and gets all the JSON data.
+    Then it renders the template for the search_results.html webpage
+    along with the query (genre string) and the movie data (JSON file)
+    where Jinja2 is implemented to display the data (don't worry about
+    this part).
+
+    Returns:
+        Any: If condition is true then we render the search_results.
+        html page. Otherwise we return to the index page.
+    """
     search_query = request.args.get('query', '')
     movie_ids = main.get_movies_by_genre(search_query)
     if movie_ids != 0:
@@ -35,6 +83,16 @@ def search_results() -> Any:
 
 @app.route('/clear_db', methods=['POST'])
 def clear_db() -> Any:
+    """ This fucntion just allows for the clearing of the mongodb
+    database when the button on the search_results.html page is
+    pressed. This particular function decleration can only be
+    accessed if a POST has been made thus clearing the database
+    in mongodb of all the data and return the user to the index
+    page.
+
+    Returns:
+        Any: Redirect to the index function
+    """
     main.remove_from_db()
     return redirect(url_for('index'))
 
--- program/main.py
+++ program/main.py
@@ -1,3 +1,11 @@
+""" Main file that operates as the "middle-man" form the
+f_app file and the database file. Where the API connection
+happens and gathers data from the TMDB and then stores
+the JSON data into Mongodb.
+
+    Returns:
+        _type_: Any, Lists, Dicts, Str, Ints
+    """
 import sys
 import os
 from typing import Any
@@ -6,6 +14,12 @@ import requests  # type: ignore
 from tmdbv3api import TMDb
 from tmdbv3api import Genre
 from . import database as db
+
+# This section just connects my api key that I
+# created for the movie database. I changed the
+# config file to environment variables so that
+# I can push this to a public repo and not give
+# away sensitive information.
 
 load_dotenv()
 tmdb_api_key = os.getenv('TMDB_API_KEY')
@@ -16,11 +30,35 @@ tmdb.debug = True
 
 
 def check_genre_title(genre: str) -> str:
+    """Capitalize first character of each word
+
+    Args:
+        genre (str): The genre (str) that is passed 
+        in is just capitilized for the first char
+        so is matches TMDB's setup
+    Returns:
+        str: New string with first char capitilized
+    """
     genre_title = genre.title()
     return genre_title
 
 
 def find_genre_id(genre: str) -> int:
+    """Once the genre has been capitilized this 
+    function sets the genre_list to class Genre
+    which in turn allows us to get the movie genre
+    list. Allowing us to find the corresponding id
+    value that TMDB uses.
+
+    Args:
+        genre (str): Compares genre against the genre_list
+        to find if a they are identical then setting the id
+        value (int) to the genre_id
+
+    Returns:
+        int: genre_id equals the integer value and returns said
+        value for discovering movies with that genre
+    """
     genre_id = 0
     genre_list = Genre()
     genre_list = genre_list.movie_list()
@@ -32,10 +70,32 @@ def find_genre_id(genre: str) -> int:
 
 
 def remove_from_db() -> Any:
+    """Goes to the database module where the insertion
+    into the Mongodb database is conducted
+
+    Returns:
+        Any: No return
+    """
     db.delete_db()
 
 
 def get_movies_by_genre(genre: str) -> Any:
+    """Checks if genre_id is an actual valid id from the
+    TMDB. If so it sets the url beginning for discovering
+    movies from the website. Then sets specific parameters and
+    headers for structuring the entire url - 
+    (https://api.themoviedb.org/3/discover/
+    movie?...paramstuff...headers...). Then sends a request and
+    stores in the response var then making it a json structure.
+    Then stores as a list of dicts to be inserted into Mongodb.
+
+    Args:
+        genre (str): The genre that we are searching for
+
+    Returns:
+        Any: Retruns either a list of id's from mongodb or
+        a 0 to indicate there was no matching id for that genre
+    """
     genre = check_genre_title(genre)
     genre_id = find_genre_id(genre)
     if genre_id != 0:
@@ -69,9 +129,24 @@ def get_movies_by_genre(genre: str) -> Any:
 
 
 def return_movie_data(movie_ids: list) -> Any:  # type: ignore
+    """Movie collection variable gets the data from the mongodb
+    collection in the cluster.
+
+    Args:
+        movie_ids (list): Passing in the list of movie ids,
+        so mongodb can return the movie data stored in the
+        cluster
+
+    Returns:
+        Any: Returns a list of dicts with all the JSON data 
+        that was stored in the mongodb database
+    """
     movie_collection = db.get_documents(movie_ids)
     return movie_collection
 
+# These functions below are not used, only for testing
+# purposes for when I was setting up the api and the
+# the connection to Mongodb.
 
 def get_input() -> str:
     sys.stdout.write("Enter a genre to search for: ")
