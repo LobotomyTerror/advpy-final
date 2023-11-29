6013e028b8a00f6f9c214b929621bbc597fc2ad2
Daniel <dfishbein@mavs.coloradomesa.edu>
2023-11-29 07:55:01 -0700

    quick change

--- program/config.py
+++
@@ -1,8 +0,0 @@
-MONGODB_USRNM = "dfishbein"
-MONGODB_PASS = "pOXhLTkqvtMlHKQW"
-TMDB_API_KEY = 'ddc68a76526100939a2f7c62b4f6b061'
-TMDB_AUTH_KEY = (
-    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZGM2OGE3NjUyNjEwMDkzOWEyZ"
-    "jdjNjJiNGY2YjA2MSIsInN1YiI6IjY1M2FjOTMzY2M5NjgzM"
-    "DEyY2YyOGQ0YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2"
-    "ZXJzaW9uIjoxfQ.WmdpACOzdcshp_a9LgnkU6Pj67ora5k5tM8zrA46_gc")
