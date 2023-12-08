363216eab7a6fefa1e8cf60295e34b61dff72940
Daniel <dfishbein@mavs.coloradomesa.edu>
2023-12-08 05:37:35 +0000

    Added some stuff to the test_main file

--- test/test_main.py
+++ test/test_main.py
@@ -1,7 +1,10 @@
 import unittest
+from pymongo import MongoClient, errors
 from program.main import check_genre_title
 from program.main import find_movie_genre_id
 from program.main import find_tv_genre_id
+from program.database import create_mongo_client
+from program.database import get_uri
 
 
 class test_main(unittest.TestCase):
@@ -30,6 +33,20 @@ class test_main(unittest.TestCase):
     def test_find_tv_genre_id(self) -> None:
         self.assertEqual(find_tv_genre_id("Drama"), 18)
         self.assertEqual(find_tv_genre_id("Comedy"), 35)
+
+    def test_mongodb_response1(self) -> None:
+        uri = get_uri()
+        result = create_mongo_client(uri)
+        self.assertIsInstance(result, MongoClient)
+
+    def test_mongodb_response2(self) -> None:
+        try:
+            uri = ''
+            # Purposely not using result just for testing purposes
+            result = create_mongo_client(uri)
+
+        except errors.ConfigurationError:
+            self.assertRaises(errors.ConfigurationError)
 
 
 if __name__ == "__main__":
